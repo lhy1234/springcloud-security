2ce10e50343b54177bc9d43f04011273f8ecf096
lhy1234 <13298457669@163.com>
2019-11-03 19:09:08 +0800

    a

--- nb-server-gateway/src/test/java/Test.java
+++ nb-server-gateway/src/test/java/Test.java
@@ -1,17 +1,21 @@
+import com.nb.security.NbServerGatewayApplication;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = NbServerGatewayApplication.class)
 public class Test {
 
-//    @Autowired
-//    private RedisTemplate<String,Object> redisTemplate;
+    @Autowired
+    private RedisTemplate<String,Object> redisTemplate;
 
-//
-//    @org.junit.Test
-//    public void test(){
-//        redisTemplate.opsForValue().set("abc",123);
-//        System.err.println("aasdjjalskdjl");
-//    }
+
+    @org.junit.Test
+    public void test(){
+        redisTemplate.opsForValue().set("abc",123);
+        System.err.println("aasdjjalskdjl");
+    }
 }
